6077461d3391db7bdfc070bc146a477140c6e1da
WillJwitz <william.jurewitz12@gmail.com>
2025-04-20 17:23:21 -0700

    Main Menu gui

--- interfaceComponent.py
+++ interfaceComponent.py
@@ -1,10 +1,11 @@
 import tkinter as tk
+from tkinter import ttk
 from memoryServerComponent import memory_server_component
 import subprocess
 import os
 
 class app_window(tk.Tk):
-    """Class to define the application window.
+    """ Class to define the application window.
         Different menus will be contained in 
         tkinter 'frames' which can be understood
         as a 'layer' of the display. Screen
@@ -48,27 +49,29 @@ class login(tk.Frame):
         super().__init__(container)
         self.application = app
 
-        self.entryField = tk.Frame(self)
+        self.entry_field = tk.Frame(self)
 
-        self.prompt = tk.Label(self.entryField, text="Enter Username")
+        self.prompt = tk.Label(self.entry_field, text="Enter Username")
         self.prompt.pack()
 
         self.username = ""
-        self.userField = tk.Entry(self.entryField,textvariable=self.username )
-        self.userField.pack()
+        self.user_field = tk.Entry(self.entry_field,textvariable=self.username )
+        self.user_field.pack()
 
-        self.error = tk.Label(self.entryField, text="")
+        self.error = tk.Label(self.entry_field, text="")
         self.error.pack()
 
-        self.entryField.pack(expand=True)
+        self.entry_field.pack(expand=True)
 
-        self.userField.bind("<Return>", self.submit_user)
-        self.userField.focus_set()
+        self.user_field.bind("<Return>", self.submit_user)
+        self.user_field.focus_set()
 
     def submit_user(self, event=None):
         print("ENTER")
+        user_entered = self.user_field.get()
         # HAYDEN
-        # Use servercomponent to set user
+        # Use servercomponent to set user and get boolean
+        # On server succes:
         # Use main_menu.set_user to change user for that class
         # use self.application to access main_menu
 
@@ -81,11 +84,92 @@ class login(tk.Frame):
 
 class main_menu(tk.Frame):
     def __init__(self, container, app):
+
+        # Establish Parent and essential vars
         super().__init__(container)
+        self.application = app
         self.current_user = "temp"
 
-        self.user_label = tk.Label(self, text=self.current_user)
-        self.user_label.pack(fill="both", expand=True )
+        self.pdf = "" # str for pdf name
+        self.pdf_path = "" # str for pdf path
+        self.note_json = None# var for note json object
+        
+
+        # Configure grid to have top and sidebar margins
+        self.grid_rowconfigure(0, minsize = 20)
+        self.grid_rowconfigure(1, weight = 1)
+        self.grid_columnconfigure(0, minsize = 20)
+        self.grid_columnconfigure(1, weight = 1)
+
+        # Header text setup and display
+        self.header_text = f"Welcome {self.current_user}. Select a PDF and Note to begin!"
+        self.header = tk.Label(self, text=self.header_text, font=('Times New Roman', 18))
+        self.header.grid(row=0,column=1,sticky="ew")
+
+        # Menu button setup and display
+        self.menu_graphic = tk.PhotoImage(file="Assets\\Menu\\menuButton.png")
+        self.menu_graphic = self.menu_graphic.subsample(4,4)
+        self.logout_button = tk.Button(self, image=self.menu_graphic)
+        self.logout_button.grid(row=0,column=0)
+
+        # Frame setup for selections
+        self.note_frame = tk.Frame(self)
+        self.note_frame.grid_rowconfigure(0, weight=1) # Setup three rows
+        self.note_frame.grid_rowconfigure(1, weight=1)
+        self.note_frame.grid_rowconfigure(2, weight=1)
+        self.note_frame.grid_columnconfigure(0, weight=1) # Setup one column
+        self.note_frame.grid(row=1,column=1,sticky="nsew")
+
+        # PDF selector setup
+        self.pdf_options = self.application.server.get_pdfs()
+        self.pdf_select = ttk.Combobox(self.note_frame, values=self.pdf_options, state="readonly")
+        self.pdf_select.grid(row=0,column=0,padx=50,sticky="ew")
+        self.pdf_select.set("--Select a PDF--")
+        self.pdf_select.bind("<<ComboboxSelected>>", self.pdf_selected)
+
+        # Note selector setup
+        self.note_options = []
+        self.note_select = ttk.Combobox(self.note_frame, values=self.note_options, state="read")
+        self.note_select.grid(row=1,column=0,padx=50,sticky="ew")
+        self.note_select.set("Select a PDF First")
+        self.note_select.bind("<<ComboboxSelected>>", self.note_selected)
+
+        # Button setup
+        self.butt_frame = tk.Frame(self.note_frame)
+        self.butt_frame.grid(row=2,column=0,sticky="nsew")
+        self.butt_frame.grid_rowconfigure(0, weight=1)
+        self.butt_frame.grid_columnconfigure(0, weight=1)
+        self.butt_frame.grid_columnconfigure(1, weight=1)
+        # Show PDF button --> open pdf without booting into editor
+        self.pdf_button = tk.Button(self.butt_frame, text="Open PDF", command=self.show_pdf)
+        self.pdf_button.grid(row=0,column=0,sticky="ew", padx=50)
+        # Open Note button --> open note in note taking GUI
+        self.note_button = tk.Button(self.butt_frame, text="Open Note", command=self.open_note)
+        self.note_button.grid(row=0,column=1,sticky="ew",padx=50)
+
+    def open_note(self, event=None):
+        # No GUI right now so this will just pass
+        pass
+
+    def show_pdf(self, event=None):
+        # HAYDEN
+        # This relies on the pdf_selected() being implemented properly
+        cwd = os.getcwd()
+
+        #Open PDF in user's default viewer.
+        subprocess.Popen([cwd+self.pdf_path], shell=True)
+
+    def note_selected(self, event=None):
+        note_name = self.note_select.get()
+        print(f"Selected note {note_name}")
+
+    def pdf_selected(self, event=None):
+        pdf_name = self.pdf_select.get()
+        print(f"Selected {pdf_name}")
+
+        # HAYDEN
+        # Need to set the pdf and pdf_path vars local to main menu here
+        # Also need to update note_options using server call
 
     def set_user(self, name):
         self.current_user = name
@@ -93,7 +177,7 @@ class main_menu(tk.Frame):
 def main():
     serverObject = memory_server_component()
 
-    win =  app_window(serverObject)
+    win = app_window(serverObject)
     win.mainloop()
     
 
